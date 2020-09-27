60683d7414ee20b2ffe34eea3acd393ead62372d
Mai Yia Xiong <maiyiax@gmail.com>
2020-09-27 03:23:09 -0500

    added questions to script

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// list of questions
+var questions = [
+    {
+        question : "What does CSS stand for?",
+        choiceA : "Computer Style Sheets",
+        choiceB : "Cascading Style Sheets",
+        choiceC : "Compiled Style sheets",
+        answer : "B"
+
+    },
+    {
+        question : "An array is enclosed within?",
+        choiceA : "Curly Braces",
+        choiceB : "Square Brackets",
+        choiceC : "Parantheses",
+        answer : "B"
+
+    },
+    {
+        question : "Which data type returns a value of true or false.",
+        choiceA : "Boolean",
+        choiceB : "String",
+        choiceC : "Int",
+        answer : "A"
+
+    },
+    {
+        question : "Which of the following is not an HTML semantic element? ",
+        choiceA : "<article>",
+        choiceB : "<form>",
+        choiceC : "<span>",
+        answer : "C"
+
+    },
+    {
+        question : "Which html element is used to create an unordered list?",
+        choiceA : "ul",
+        choiceB : "ol",
+        choiceC : "li",
+        answer : "A"
+
+    },
+]
