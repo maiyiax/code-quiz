27f06c2d6c2fe6ee5538768de3f53b29a466a51e
Mai Yia Xiong <maiyiax@gmail.com>
2020-09-27 22:32:44 -0500

    added functions to display questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,57 @@
 // array of questions
 var questions = [
     {
-        question : "What does CSS stand for?",
+        q : "What does CSS stand for?",
         choiceA : "Computer Style Sheets",
         choiceB : "Cascading Style Sheets",
         choiceC : "Compiled Style sheets",
-        answer : "B"
+        a : "B"
 
     },
     {
-        question : "An array is enclosed within?",
+        q : "An array is enclosed within?",
         choiceA : "Curly Braces",
         choiceB : "Square Brackets",
         choiceC : "Parantheses",
-        answer : "B"
+        a: "B"
 
     },
     {
-        question : "Which data type returns a value of true or false.",
+        q : "Which data type returns a value of true or false.",
         choiceA : "Boolean",
         choiceB : "String",
         choiceC : "Int",
-        answer : "A"
+        a : "A"
 
     },
     {
-        question : "Which of the following is not an HTML semantic element? ",
+        q : "Which of the following is not an HTML semantic element? ",
         choiceA : "<article>",
         choiceB : "<form>",
         choiceC : "<span>",
-        answer : "C"
+        a : "C"
 
     },
     {
-        question : "Which html element is used to create an unordered list?",
+        q : "Which html element is used to create an unordered list?",
         choiceA : "ul",
         choiceB : "ol",
         choiceC : "li",
-        answer : "A"
+        a : "A"
 
     },
 ]
 
-// list variables
+// list the variables
 var timerEl = document.getElementById('timer');
 var BtnEl = document.getElementById("startBtn");
-
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var finalQuestion = questions.length - 1;
+var currentQuestion = 0;
+var question = document.getElementById("question");
+var score = 0;
 
 // function that will start the quiz and timer countdown
 function countDown() {
@@ -65,19 +71,55 @@ function countDown() {
         } else {
             timerEl.textContent = 'Time Up!';
             clearInterval(timer);
+            // show score
         }
        
 
     }, 1000);
 }
 
-// present the questions
-//function quizQuestions() {
-  //  let q = questions[];
-    //question.innerHTML = "<p>" + q.questions + "</p>";
-    //choiceA.innerHTML = q,choiceA;
-    //choiceB.innerHTML = q.choiceB;
-    //choiceC.innerHTML = q.choiceC;
-//}
+// Present the quiz questions
+function askQuestion () {
+    var q = questions[currentQuestion];
+
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+function showQuestion () {
+    // remove the initial display
+    quizSection.style.display = "none";
+    askQuestion();
+    quiz.style.display = "block";
+    loopQuestions();
+    countDown();
+}
+
+// create a loop to go through the array
+function loopQuestions () {
+    currentQuestion++;
+
+    if (currentQuestion < finalQuestion) {
+        endQuiz();
+        return;
+    } else {
+        quiz.innerHTML += "<div class='loop' id=" + currentQuestion +"></div>";
+    }
+}
+
+// display a message with the score when the game is over
+function endQuiz () {
+    clearInterval(timer);
+
+    var results = `
+    <h2>Quiz Over!</h2>
+    <h2>Your final score is ` + score + ` /100!</h2>
+    <input type-"text" id="name" placeholder="Enter your name">
+    <button onclick="saveScore()">Save Score</button>`;
+
+    document.getElementById("quizSection").innerHTML = results;
+}
 // add an event listener for when the start button is clicked
-BtnEl.addEventListener("click",countDown);
+BtnEl.addEventListener("click",showQuestion);
