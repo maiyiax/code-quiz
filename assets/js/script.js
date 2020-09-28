20818104582f130193ae7905f78ae3e2b753d20e
Mai Yia Xiong <maiyiax@gmail.com>
2020-09-27 19:26:44 -0500

    updated timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,4 +43,41 @@ var questions = [
 ]
 
 // list variables
-var timer = 
+var timerEl = document.getElementById('timer');
+var BtnEl = document.getElementById("startBtn");
+
+
+// function that will start the quiz and timer countdown
+function countDown() {
+    
+    // set the initial time
+    var timeLeft = 5;
+
+    // start the countdown using setInterval method and setting the conditions
+
+    var timer = setInterval(function() {
+        if (timeLeft > 1) {
+            timerEl.textContent = 'Time Left: ' + timeLeft;
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = 'Time Left: ' + timeLeft;
+            timeLeft--;
+        } else {
+            timerEl.textContent = 'Time Up!';
+            clearInterval(timer);
+        }
+       
+
+    }, 1000);
+}
+
+// present the questions
+//function quizQuestions() {
+  //  let q = questions[];
+    //question.innerHTML = "<p>" + q.questions + "</p>";
+    //choiceA.innerHTML = q,choiceA;
+    //choiceB.innerHTML = q.choiceB;
+    //choiceC.innerHTML = q.choiceC;
+//}
+// add an event listener for when the start button is clicked
+BtnEl.addEventListener("click",countDown);
